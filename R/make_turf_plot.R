ad4ed5e36dd8b8e90fb78d1b8aab848e40eabd32
RichardJTelford <Richard.Telford@uib.no>
2024-05-24 18:18:55 +0200

    make maps for data without subturfs

--- R/make_turf_plot.R
+++ R/make_turf_plot.R
@@ -1,44 +1,46 @@
-#'@title Subturf maps over time
-#'@param data Community data.frame in long form
-#'@param grid_long grid from \code{make_grid}
-#'@param title character; text for title
-#'@param species name of column with species data
-#'@param cover name of column with cover data
-#'@param year name of column with year data
-#'@param subturf name of column with subturf IDs 
-#'@details See vignette for examples
-#'@return a ggplot object
-#'@importFrom ggplot2 ggplot aes geom_tile facet_grid ggtitle 
-#'@importFrom ggplot2 scale_x_continuous scale_y_continuous scale_fill_distiller
-#'@importFrom ggplot2 theme_bw theme element_blank unit element_text
-#'@importFrom rlang .data
-#'@importFrom dplyr left_join rename
-#'@export
+#' @title Subturf maps over time
+#' @param data Community data.frame in long form
+#' @param grid_long grid from \code{make_grid}
+#' @param title character; text for title
+#' @param species name of column with species data
+#' @param cover name of column with cover data
+#' @param year name of column with year data
+#' @param subturf name of column with subturf IDs
+#' @details See vignette for examples
+#' @return a ggplot object
+#' @importFrom ggplot2 ggplot aes geom_tile facet_grid ggtitle
+#' @importFrom ggplot2 scale_x_continuous scale_y_continuous scale_fill_distiller
+#' @importFrom ggplot2 theme_bw theme element_blank unit element_text
+#' @importFrom rlang .data
+#' @importFrom dplyr left_join rename
+#' @export
 
-make_turf_plot <- function(data, grid_long, species, cover, year, subturf, title){
-  #rename columns for convenience
-  data <- rename(data, subturf = {{subturf}}, species = {{species}}, year = {{year}})
-  
-  #check no unidentified subturfs
+make_turf_plot <- function(data, grid_long, species, cover, year, subturf, title) {
+  # rename columns for convenience
+  data <- rename(data, subturf = {{ subturf }}, species = {{ species }}, year = {{ year }})
+
+  # check no unidentified subturfs
   stopifnot(all(data$subturf %in% grid_long$subturf))
 
-  #join data to grid  
+  # join data to grid
   data <- left_join(data, grid_long, by = "subturf")
-  
-  #plot
-  ggplot(data, aes(x = .data$.x, 
-                   y = .data$.y, 
-                   fill = {{cover}})) +
+
+  # plot
+  ggplot(data, aes(
+    x = .data$.x,
+    y = .data$.y,
+    fill = {{ cover }}
+  )) +
     geom_tile(colour = "grey60") +
     facet_grid(species ~ year) +
     ggtitle(title) +
     scale_x_continuous(expand = c(0, 0)) +
     scale_y_continuous(expand = c(0, 0)) +
-    scale_fill_distiller(type = "seq", palette = "Greens", direction = 1) + 
+    scale_fill_distiller(type = "seq", palette = "Greens", direction = 1, limits = c(0, NA)) +
     theme_bw() +
     theme(
-      axis.text = element_blank(), 
-      axis.title = element_blank(), 
+      axis.text = element_blank(),
+      axis.title = element_blank(),
       axis.ticks = element_blank(),
       panel.spacing.y = unit(0.04, "lines"),
       panel.grid.minor = element_blank(),
---
+++ R/make_turf_plot_no_subturfs.R
@@ -0,0 +1,29 @@
+#' Turf map over time
+#' @description
+#' Plot species cover against time when there are no subturf information
+#' @param data Community data.frame in long form
+#' @param title character; text for title
+#' @param species name of column with species data
+#' @param cover name of column with cover data
+#' @param year name of column with year data
+#' @details This is just a wrapper for a simple ggplot, but made consistent with See vignette for examples
+#' @return a ggplot object
+#' @importFrom ggplot2 ggplot aes geom_tile facet_grid ggtitle
+#' @importFrom ggplot2 scale_x_continuous scale_y_discrete scale_fill_distiller
+#' @importFrom ggplot2 theme_bw theme element_blank
+#'
+#' @export
+
+make_turf_plot_no_subturfs <- function(data, year, species, cover, title) {
+  data |>
+    ggplot(aes(x = {{ year }}, y = {{ species }}, fill = {{ cover }})) +
+    geom_tile() +
+    ggtitle(title) +
+    scale_x_continuous(expand = c(0, 0), breaks = scales::extended_breaks(w = c(0.5, 0.2, 0.5, 0.05))) +
+    scale_y_discrete(expand = c(0, 0)) +
+    scale_fill_distiller(type = "seq", palette = "Greens", direction = 1, limits = c(0, NA)) +
+    theme_bw() +
+    theme(
+      axis.title = element_blank()
+    )
+}
